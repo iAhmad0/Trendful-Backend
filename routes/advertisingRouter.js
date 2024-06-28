9ca87a839345fd1f9a5ca3fe33f08888cc4c28b5
mohamed sayed <ma1982001@gmail.com>
2024-06-28 23:39:16 +0200

    add advertising system

---
+++ routes/advertisingRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {addProduct,getMainPageAds,getSearchPageAds,getMainPageAdsRandom} = require("../controllers/advertisingController")
+
+router.post("/advertising/add-product",addProduct)
+
+router.get("/advertising/get-main-page-ads",getMainPageAds)
+router.get("/advertising/get-Search-page-ads",getSearchPageAds)
+//get random 5 products
+router.get("/advertising/get-main-page-ads/random",getMainPageAdsRandom)
+router.get("/advertising/get-Search-page-ads/random",getSearchPageAds)
+
+module.exports = router;
